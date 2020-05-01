c063bccc9a6370aed981db5446fe17c6048b338b
Vipyatnitsky <vitalp1982@mail.ru>
2020-05-01 22:08:00 +0300

    add function and testing works

--- app/src/main/java/com/vital/android_pyatnitskiy_121/MainActivity.java
+++ app/src/main/java/com/vital/android_pyatnitskiy_121/MainActivity.java
@@ -22,12 +22,33 @@ public class MainActivity extends AppCompatActivity {
         //                System.out.println("Hi");
         //            }
         //        });
+    }
+
+    public void registration(View view) {
+        final TextView login = findViewById(R.id.userEmail);
+        final TextView password = findViewById(R.id.userPassword);
+        final TextView log_message = findViewById(R.id.log_message);
+        if(isEmailValid(login.getText()) && password.getText().length() > 0) {
+            log_message.setText("Учетная запись создана" +
+                    "\nлогин пользователя: " + login.getText() +
+                    "\nустановлен пароль: " + password.getText());
+        } else {
+            log_message.setText("Пожалуйста укажите корректный адрес электронной почты и пароль");
+        }
 
     }
 
-    public void print(View view) {
-        final TextView text = findViewById(R.id.textView_test);
-        text.setText("Hi");
-        System.out.println("Hi");
+    public void clearForm(View view) {
+        final TextView login = findViewById(R.id.userEmail);
+        final TextView password = findViewById(R.id.userPassword);
+        final TextView log_message = findViewById(R.id.log_message);
+        login.setText("");
+        password.setText("");
+        log_message.setText("");
+
+    }
+
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
