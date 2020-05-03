59c22e76654dd85e976947851e353b3f6f4b3304
Vipyatnitsky <vitalp1982@mail.ru>
2020-05-03 10:58:51 +0300

    check errors make string resource with placeholder init variable in onCreate clear code check layout xml

--- app/src/main/java/com/vital/android_pyatnitskiy_121/MainActivity.java
+++ app/src/main/java/com/vital/android_pyatnitskiy_121/MainActivity.java
@@ -4,44 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView login;
+    private TextView password;
+    private TextView log_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+      }
 
-        //        метод привязки по id и установки слушателя событий
-        //        Button button = findViewById(R.id.button_test);
-        //        button.setOnClickListener(new View.OnClickListener() {
-        //            @Override
-        //            public void onClick(View v) {
-        //                System.out.println("Hi");
-        //            }
-        //        });
+    private void init() {
+        login = findViewById(R.id.userEmail);
+        password = findViewById(R.id.userPassword);
+        log_message = findViewById(R.id.log_message);
     }
 
     public void registration(View view) {
-        final TextView login = findViewById(R.id.userEmail);
-        final TextView password = findViewById(R.id.userPassword);
-        final TextView log_message = findViewById(R.id.log_message);
+
         if(isEmailValid(login.getText()) && password.getText().length() > 0) {
-            log_message.setText("Учетная запись создана" +
-                    "\nлогин пользователя: " + login.getText() +
-                    "\nустановлен пароль: " + password.getText());
+            log_message.setText(getString(R.string.create_user_msg, login.getText(), password.getText()));
         } else {
-            log_message.setText("Пожалуйста укажите корректный адрес электронной почты и пароль");
+            log_message.setText(R.string.error_create_user_msg);
         }
 
     }
 
     public void clearForm(View view) {
-        final TextView login = findViewById(R.id.userEmail);
-        final TextView password = findViewById(R.id.userPassword);
-        final TextView log_message = findViewById(R.id.log_message);
         login.setText("");
         password.setText("");
         log_message.setText("");
